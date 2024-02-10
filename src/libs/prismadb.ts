33509364669432b90aa6c1241f369388f9021af9
al-Reza89 <alialreza98@gmail.com>
2024-02-10 13:49:13 +0600

    build: connect prisma with mongodb

---
+++ src/libs/prismadb.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+// prisma client sob jaigay hard code kore o kora jai but error dibe next js its best practice
+
+import { PrismaClient } from '@prisma/client';
+
+// given global defination of prisma
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  var prisma: PrismaClient | undefined;
+}
+
+// ekta client create korlam hoy eita global prisma ke khujbe ba new prisma client create  korbe
+
+const client = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') global.prisma = client;
+
+export default client;
