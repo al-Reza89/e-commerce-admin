53f103fcd256f63ef7ab52c5f0de0b5e8c1af723
al-Reza89 <alialreza98@gmail.com>
2024-02-12 16:55:57 +0600

    fix: fix a issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
         </Container>
         <Guide />
         <Container>
-          <div className="pt-10  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 xl:gap-8 gap-4 pb-8  ">
+          <div className="pt-10  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 xl:gap-8 gap-4 pb-8  ">
             {allProducts.map((product: Product) => {
               return (
                 <FeaturedProducts
